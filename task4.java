facde475b7763df134fd0ce4903397cdb175d630
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-08 11:35:39 -0700

    Finished task 4

--- task4.java
+++ task4.java
@@ -1,1 +1,26 @@
-
+import java.util.Scanner ;
+public class task4
+{
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+		System.out.println("Enter an Integer: ");
+		int number = input.nextInt();
+		System.out.println("Pattern I ");
+		System.out.println("--------- ");
+	  for (int i = 1; i <= number; ++i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print(j + " ");
+      }
+      System.out.println("");
+    }
+    System.out.println(" ");
+	System.out.println("Pattern II ");
+	System.out.println("---------- ");
+	  for (int i = number; i >= 1; --i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print(j + " ");
+      }
+      System.out.println();
+    }
+	}
+}
