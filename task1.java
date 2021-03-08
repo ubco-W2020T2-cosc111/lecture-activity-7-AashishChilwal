e21e9227956643d9b36a43462f4a3f7345e26bd7
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-08 15:59:49 -0700

    Finished task 1

--- task1.java
+++ task1.java
@@ -1,1 +1,11 @@
-
+public class task1
+{
+	public static void main(String[] args) {
+	int number=0 ;
+	while (number*number < 12000)
+	{
+	    number++ ;
+	}
+	System.out.println ("The smallest integer such that its square is greater than 12,000 is: " + number);
+	}
+}
