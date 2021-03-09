a16bc247797756bc18759efaa9b4f71a4202c72a
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-08 18:55:19 -0700

    Finished Task 2

--- task2.java
+++ task2.java
@@ -1,12 +1,21 @@
+
 import java.util.Scanner;
 public class task2
 {
 	public static void main(String[] args) {
 	int vowels=0,capital_letter = 0 ;
-	String reverse = "";
+	String reverse ="";
 	Scanner vw = new Scanner(System.in);
 	System.out.println ("Enter a string: " );
 	String str = vw.nextLine();
+	char h[]=str.toCharArray();
+	int len=str.length();
+	
+		for(int j=len-1;j>=0;--j)
+		{
+		   reverse=reverse+h[j];
+
+		}
 	for (int i = 0; i < str.length(); i++) {
 	char ch = str.charAt(i);
 	    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
@@ -17,7 +26,7 @@ public class task2
         capital_letter ++ ;
     }
 	}
-	    //System.out.println("The reversed version of the string is: " + reverse);
+	    System.out.println("The reversed version of the string is: " + reverse);
 	    System.out.println ("The number of vowels in the string is: " + vowels);
 		System.out.println ("The number of Uppercase letters in the string is: " + capital_letter);
 	    
