30e7c63845e3fe221d9f52ecb59cddc3d090be07
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-08 18:03:47 -0700

    Update task2.java

--- task2.java
+++ task2.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class task2
 {
 	public static void main(String[] args) {
-	int vowels=0 ;
+	int vowels=0,capital_letter = 0 ;
+	String reverse = "";
 	Scanner vw = new Scanner(System.in);
 	System.out.println ("Enter a string: " );
 	String str = vw.nextLine();
@@ -11,8 +12,15 @@ public class task2
 	    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
         ++vowels;
       }
+    if (ch >= 65 && ch <=90)
+    {
+        capital_letter ++ ;
+    }
 	}
-	System.out.println (vowels);
+	    //System.out.println("The reversed version of the string is: " + reverse);
+	    System.out.println ("The number of vowels in the string is: " + vowels);
+		System.out.println ("The number of Uppercase letters in the string is: " + capital_letter);
+	    
 	}
 }
 
