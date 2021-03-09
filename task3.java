6a4bed2143fefe348479fc63a8e9199d2d845837
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-08 18:49:30 -0700

    Update task3.java

--- task3.java
+++ task3.java
@@ -3,22 +3,24 @@ public class task3
 {
 	public static void main(String[] args) {
 	    Scanner num = new Scanner(System.in);
+	    double gpa = 0.0;
 	    Scanner gpa_1 = new Scanner(System.in);
 		System.out.println("Enter the number of students (1 to 50 ): ");
 		int number = num.nextInt();
 		for (int i = 1; i <= number ; i++)
 		{
 		    System.out.println("Enter the Gpa of Student " + i + " : " );
-		    double gpa = gpa_1.nextDouble();
-		    if (gpa < 0 && gpa > 4)
+		    gpa = gpa_1.nextDouble();
+		    if (gpa < 0.0 || gpa > 4.0)
 		    {
              System.out.println("Invalid GPA value. Try again: " );
              gpa = gpa_1.nextDouble();
-		}
+            		}
 		else 
 		{
 		    continue;
 		}
 	}
+	System.out.println("The highest Gpa in the class is: " + gpa);
 }
 }
