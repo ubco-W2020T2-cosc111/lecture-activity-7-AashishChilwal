ed9b963ec3019717508df34433c2c8f82f6ce74a
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-08 17:20:31 -0700

    Partly done with task3

--- task3.java
+++ task3.java
@@ -1,1 +1,24 @@
-
+import java.util.Scanner;
+public class task3
+{
+	public static void main(String[] args) {
+	    Scanner num = new Scanner(System.in);
+	    Scanner gpa_1 = new Scanner(System.in);
+		System.out.println("Enter the number of students (1 to 50 ): ");
+		int number = num.nextInt();
+		for (int i = 1; i <= number ; i++)
+		{
+		    System.out.println("Enter the Gpa of Student " + i + " : " );
+		    double gpa = gpa_1.nextDouble();
+		    if (gpa < 0 && gpa > 4)
+		    {
+             System.out.println("Invalid GPA value. Try again: " );
+             gpa = gpa_1.nextDouble();
+		}
+		else 
+		{
+		    continue;
+		}
+	}
+}
+}
